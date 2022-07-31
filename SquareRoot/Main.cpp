e6cebdeb402f8ef80c27a419cb32e79edc660674
Rahul Walse <rahul_walse@hotmail.com>
2022-07-31 21:49:33 +0530

    Provided menu to the user.

--- SquareRoot/Main.cpp
+++ SquareRoot/Main.cpp
@@ -1,13 +1,36 @@
-// Tester file
+/*
+	Tester file to find the square root of given number and various other operations.
+*/
 
 #include <iostream>
 #include "SquareRoot.h"
 
 int main()
 {
-	std::cout << "\nEnter a number to find square root of: ";
+	bool stay = true;
+	int option = 0;
 	double number = 0;
-	std::cin >> number;
 
-	std::cout << "\nSquare root of " << number << " is " << square_root(number) << std::endl;
+	// Menu options
+	while (stay)
+	{
+		std::cout << "\n1. Find square root of a number\n2. Exit";
+		std::cout << "\nChoose an option: ";
+		std::cin >> option;
+
+		switch (option)
+		{
+			case 1:	std::cout << "\nEnter a number to find square root of: ";
+					std::cin >> number;
+					std::cout << "\nSquare root of " << number << " is " << square_root(number) << std::endl;
+					break;
+
+			case 2:	std::cout << "\nExiting the program...\n";
+					stay = false;
+					break;
+
+			default:std::cout << "\nInvalid choice! Please choose from the options provided.\n";
+					break;
+		}
+	}
 }
