8993965559576eb0ecd3926c86b2786a2a6d8f8c
Rahul Walse <rahul_walse@hotmail.com>
2022-09-11 13:22:21 +0530

    Added option for finding square root without using averaging formula.

--- SquareRoot/Main.cpp
+++ SquareRoot/Main.cpp
@@ -1,5 +1,7 @@
 /*
-	Tester file to find the square root of given number and various other operations.
+	This program is an implementation of algorithm in C++ to find square root of given
+	number including supplementary problems and algorithms from the book "How to Solve it
+	by Computer" by R.G. Dromey.
 */
 
 #include <iostream>
@@ -10,11 +12,13 @@ int main()
 	bool stay = true;
 	int option = 0;
 	double number = 0;
+	char squareRootSymbol = 251;
 
 	// Menu options
 	while (stay)
 	{
-		std::cout << "\n1. Find square root of a number\n2. Exit";
+		std::cout << "\n1. Find square root of a number\n2. Find square root of a number (high iterations)";
+		std::cout << "\n3. Exit";
 		std::cout << "\nChoose an option: ";
 		std::cin >> option;
 
@@ -22,10 +26,17 @@ int main()
 		{
 			case 1:	std::cout << "\nEnter a number to find square root of: ";
 					std::cin >> number;
-					std::cout << "\nSquare root of " << number << " is " << square_root(number) << std::endl;
+					//std::cout << "\nSquare root of " << number << " is " << square_root(number) << std::endl;
+					std::cout << '\n' << squareRootSymbol << number << " = " << square_root(number) << '\n';
 					break;
 
-			case 2:	std::cout << "\nExiting the program...\n";
+			case 2: std::cout << "\nEnter a number to find square root of: ";
+					std::cin >> number;
+					//std::cout << "\nSquare root of " << number << " is " << square_root_iterative(number) << std::endl;
+					std::cout << '\n' << squareRootSymbol << number << " = " << square_root_iterative(number) << '\n';
+					break;
+
+			case 3:	std::cout << "\nExiting the program...\n";
 					stay = false;
 					break;
 
