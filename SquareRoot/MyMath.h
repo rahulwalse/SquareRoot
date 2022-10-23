3e48f7038ebbc32d294bfa1c8c68350f1c9ba434
Rahul Walse <rahul_walse@hotmail.com>
2022-10-23 19:16:36 +0530

    Implemented method for exponent/power functionality.

--- SquareRoot/MyMath.h
+++ SquareRoot/MyMath.h
@@ -21,4 +21,25 @@ T square(const T& number)
 	return number * number;
 }
 
+template <typename T>
+inline T exponent(T& base, T power)
+{
+	T base_raise_to_exponent = base;
+	//- If exponent is negative, make it positive and calculate 1/x^n.
+	if (power < 0)
+	{
+		power = -power;
+		base_raise_to_exponent = 1 / exponent(base, power);
+		return base_raise_to_exponent;
+	}
+	else if (power == 0)
+		return 1;	//- If exponent is 0, value will always be 1.
+	else
+	{
+		for (T iteration = 1; iteration < power; iteration++)
+			base_raise_to_exponent *= base;
+		return base_raise_to_exponent;
+	}
+}
+
 #endif // !MYMATH_H
